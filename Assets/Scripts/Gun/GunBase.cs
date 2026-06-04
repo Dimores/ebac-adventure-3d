07451c2dbeb708d13850412f503c60e556b5cd8b
Diego Morelo <xxkirito31@gmail.com>
2026-06-04 15:11:54 -0300

    feat: importing input system and starting to do player abilities

---
+++ Assets/Scripts/Gun/GunBase.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBase : MonoBehaviour
+{
+    public ProjectileBase prefabProjectile;
+
+    public Transform positionToShoot;
+    public float timeBeetweenShoot = .3f;
+
+    private Coroutine _currentCoroutine;
+
+    public KeyCode keyCode = KeyCode.Z;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            _currentCoroutine = StartCoroutine(StartShoot());
+        }
+        else if (Input.GetKeyUp(keyCode))
+        {
+            if(_currentCoroutine != null)
+                StopCoroutine(_currentCoroutine);
+        }
+    }
+
+    IEnumerator StartShoot()
+    {
+        while (true)
+        {
+            Shoot();
+            yield return new WaitForSeconds(timeBeetweenShoot);
+        }
+    }
+
+    public void Shoot()
+    {
+        var projectile = Instantiate(prefabProjectile);
+        projectile.transform.position = positionToShoot.position;
+        projectile.transform.rotation = positionToShoot.rotation;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAbilityShoot.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerAbilityShoot : PlayerAbilityBase
+{
+    private void Update()
+    {
+
+    }
+}
+
